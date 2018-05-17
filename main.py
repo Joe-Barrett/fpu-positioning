49645d0c0508cc7daf3b4c2f8c72d2847aaf3665
JoeBarrett <joe.barrett@stfc.ac.uk>
2018-05-17 15:57:04 +0100

    Add args

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
+import argparse
 import os
+import sys
 from circle_finder import CircleFinder
 
 _image_dir = 'Commissioning/Position test 1/'
 
 
 def main():
-    files = os.listdir(_image_dir)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d',
+                        '--directory',
+                        action='store',
+                        metavar='',
+                        required=True,
+                        help='The directory to read images from')
+    parser.add_argument('-o',
+                        '--output',
+                        action='store',
+                        metavar='',
+                        required=False,
+                        default='output.txt',
+                        help='The filename for the output file, will save in provided image directory')
+    args = parser.parse_args()
+    if len(sys.argv) is 1:
+        parser.print_help()
+        sys.exit(0)
+    if args.directory[-1] is not '/':
+        args.directory = args.directory + '/'
+    print(args.directory)
     cf = CircleFinder()
-    with open('output.csv', 'w') as csv:
+    output_filepath = '/'.join([args.directory, args.output])
+    files = [f for f in os.listdir(args.directory) if '.txt' not in f]
+    with open(output_filepath, 'w') as csv:
         csv.write('file, circle, x, y\n')
         for file in files:
-            csv.write(cf.get_data(_image_dir + file) + '\n')
+            csv.write(cf.get_data(args.directory + file) + '\n')
 
 
 if __name__ == '__main__':
