7cbf8a0ebf012e661949f0d29d0c17ed642bd7a6
JoeBarrett <joe.barrett@stfc.ac.uk>
2018-05-24 15:29:19 +0100

    Change to use bounding circle, add vis mode

--- circle_finder.py
+++ circle_finder.py
@@ -4,6 +4,9 @@ import image_utils
 
 class CircleFinder:
 
+    def __init__(self, visual_mode=False):
+        self.visual_mode = visual_mode
+
     def test(self):
         image_utils.show_image(path='./Commissioning/Position test 1/a000b140.bmp')
         print('Hello from CircleFinder')
@@ -18,18 +21,18 @@ class CircleFinder:
                       key=cv2.contourArea,
                       reverse=True)[:2]
 
-    def _get_centres(self, contours):
-        centres = {}
-        for i, contour in enumerate(contours):
-            M = cv2.moments(contour)
-            circle = ''
-            if i is 0:
-                circle = 'A'
-            else:
-                circle = 'B'
-            centre = (M['m10'] / M['m00'], M['m01'] / M['m00'])
-            centres[circle] = centre
-        return centres
+    # def _get_centres(self, contours):
+    #     centres = {}
+    #     for i, contour in enumerate(contours):
+    #         M = cv2.moments(contour)
+    #         circle = ''
+    #         if i is 0:
+    #             circle = 'A'
+    #         else:
+    #             circle = 'B'
+    #         centre = (M['m10'] / M['m00'], M['m01'] / M['m00'])
+    #         centres[circle] = centre
+    #     return centres
 
     def _draw_contours(self, contours, image):
         for i, contour in enumerate(contours):
@@ -40,18 +43,39 @@ class CircleFinder:
             cv2.circle(image, (contourX, contourY), 7, (255, 255, 255), -1)
         return image
 
+    def _get_centres(self, contours, image):
+        centres = {}
+        for i, contour in enumerate(contours):
+            circle = ''
+            if i is 0:
+                circle = 'A'
+            else:
+                circle = 'B'
+            (x, y), radius = cv2.minEnclosingCircle(contour)
+            centres[circle] = (x, y)
+            center = (int(x), int(y))
+            radius = int(radius)
+            if self.visual_mode:
+                cv2.circle(image, center, radius, (0, 255, 0), 2)
+                cv2.circle(image, center, 7, (255, 255, 255), -1)
+        if self.visual_mode:
+            image_utils.show_image(image, delay=20)
+        return centres
+
     def get_data(self, path):
         image = image_utils.load_image(path)
         edged = self._edge_detect(image)
         contours = self._find_contours(edged)
-        centres = self._get_centres(contours)
+        centres = self._get_centres(contours, image)
         line_a = '{}, A, {}, {}'.format(path.split('/')[-1], centres['A'][0], centres['A'][1])
         line_b = '{}, B, {}, {}'.format(path.split('/')[-1], centres['B'][0], centres['B'][1])
         return '\n'.join([line_a, line_b])
+
 
     def find_circles(self, path):
         image = image_utils.load_image(path)
         edged = self._edge_detect(image)
         contours = self._find_contours(edged)
-        drawn = self._draw_contours(contours, image)
-        image_utils.show_image(drawn, delay=20)
+        # self._draw_bounding_circle(contours, image)
+        # drawn = self._draw_contours(contours, image)
+        # image_utils.show_image(drawn)
--- main.py
+++ main.py
@@ -21,6 +21,11 @@ def main():
                         required=False,
                         default='output.txt',
                         help='The filename for the output file, will save in provided image directory')
+    parser.add_argument('-v',
+                        '--visual',
+                        action='store_true',
+                        required=False,
+                        help='Run in visual mode')
     args = parser.parse_args()
     if len(sys.argv) is 1:
         parser.print_help()
@@ -28,9 +33,11 @@ def main():
     if args.directory[-1] is not '/':
         args.directory = args.directory + '/'
     print(args.directory)
-    cf = CircleFinder()
+    cf = CircleFinder(visual_mode=args.visual)
     output_filepath = '/'.join([args.directory, args.output])
+    print('Getting file list')
     files = [f for f in os.listdir(args.directory) if '.txt' not in f]
+    print('Running')
     with open(output_filepath, 'w') as csv:
         csv.write('file, circle, x, y\n')
         for file in files:
